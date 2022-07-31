a52b882889f591edefdb4d54c84ec51f44f07a1b
DanyaBu <daria.tymokhina@gmail.com>
2022-07-31 09:56:14 +0300

    Add date

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
+import datetime
+
 name = input("What's your name? ")
 print("Hello, ", name,"!")
 print("we learn Git")
 print("we learn Gitub")
+print("Today: ", datetime.today().strftime('%d/%m/%Y'))
